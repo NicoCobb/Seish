36563f43b468fed13403c44f7c81aae7c23a4cb2
AlexanderFrancoletti <alexander.francoletti@gmail.com>
2017-09-30 23:46:34 -0400

    pepper.lua works

--- pepper.lua
+++ pepper.lua
@@ -9,8 +9,8 @@ require "enemy"
 Pepper = Class{__includes = Enemy}
 
 function Pepper:init(x, y, target)
-	self.x = x
-	self.y = y
+	self.x = 1000
+	self.y = 600
 	self.target = target
 	self.width = 50
 	self.height = 100
@@ -20,7 +20,7 @@ function Pepper:init(x, y, target)
 	self.color = {0, 0, 255}
 	self.rX = math.random(1, 360)
 	self.rY = math.random(1, 360)
-	self.moveCooldown = 2
+	self.moveCooldown = 1
 	end
 	
 function Pepper:update(dt)
@@ -29,6 +29,7 @@ function Pepper:update(dt)
 	if self.moveCooldown <= 0 then
 		self.rX = math.random(1, 360)
 		self.rY = math.random(1, 360)
+		self.moveCooldown = 1
 		end
 	if self.moveCooldown > 0 then
 		self.moveCooldown = self.moveCooldown - dt
